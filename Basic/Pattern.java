36646668134951840f6ab2cbb90e69ba3c5dbd12
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-02 11:01:10 +0530

    updated

---
+++ Basic/Pattern.java
@@ -0,0 +1,17 @@
+class Pattern
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+
+        for(int i=0;i<a;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+}
