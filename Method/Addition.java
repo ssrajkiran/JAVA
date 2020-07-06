2ad4b8d21281036c6b10c4346dab3077a4687bfe
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-06 17:55:05 +0530

    added method with program 1

---
+++ Method/Addition.java
@@ -0,0 +1,71 @@
+import java.io.*;
+
+
+public class Addition
+{
+    static void fun(int x,int y)
+    {
+        int w=x+y;
+        System.out.println("Your addition is:" +w);
+    }
+    static void fun(int x,int y,int z)
+    {
+        int s=x+y+z;
+        System.out.println("Your addition is:"+s);
+    }
+    static void fun(int x,int y,int z,int v)
+    {
+        int a=x+y+z+v;
+        System.out.println("Your addition is:"+a);
+    }
+    public static void main(String args[])throws Exception
+    {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader input = new BufferedReader(r);
+        System.out.println("Enter how many numbers you want to add(upto 4 number):");
+        int number = Integer.parseInt(input.readLine());
+
+        if(number==1)
+        {
+            System.out.println("Your answer is:"+number);
+        }
+        if(number==2)
+        {
+            System.out.println("Enter first number:");
+            int a = Integer.parseInt(input.readLine());
+            System.out.println("Enter second number:");
+            int b =Integer.parseInt(input.readLine());
+
+            fun(a,b);
+        }
+        if(number==3)
+        {
+             System.out.println("Enter first number:");
+            int a = Integer.parseInt(input.readLine());
+            System.out.println("Enter second number:");
+            int b =Integer.parseInt(input.readLine());
+             System.out.println("Enter third number:");
+            int c = Integer.parseInt(input.readLine());
+           
+            fun(a,b,c);
+        }
+        if(number==4)
+        {
+             System.out.println("Enter first number:");
+            int a = Integer.parseInt(input.readLine());
+            System.out.println("Enter second number:");
+            int b =Integer.parseInt(input.readLine());
+             System.out.println("Enter third number:");
+            int c = Integer.parseInt(input.readLine());
+            System.out.println("Enter forth number:");
+            int d =Integer.parseInt(input.readLine());
+
+            fun(a,b,c,d);
+        }
+        else
+        {
+            System.out.println("Upto only 4 numbers not more than that...");
+        }
+        
+    }
+}
