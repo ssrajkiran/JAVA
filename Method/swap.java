39bd43546cada927d677e29d187e455115cfc1b0
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-07 10:41:58 +0530

    modified

--- Method/swap.java
+++ Method/swap.java
@@ -3,15 +3,16 @@ class swap
 {
     static void swap(int x,int y)
     {
-        int temp;
-        temp = x;
+    
+      int temp = x;
         x=y;
         y=temp;
-        System.out.println(+x,+y);
+        System.out.println(+x+" "+y);
     }
     public static void main(String args[])throws Exception
+    {
     InputStreamReader r = new InputStreamReader(System.in);
-    BufferedReader input = BufferedReader(r);
+    BufferedReader input = new BufferedReader(r);
 
     System.out.println("Swapping numbers:");
     System.out.println("Enter 1st number:");
@@ -19,6 +20,10 @@ class swap
 
     System.out.println("Enter 2nd number:");
     int b=Integer.parseInt(input.readLine());
-    System.out.println("Before swap:")
-    swap();
+    System.out.println("Before swap:");
+    System.out.println(+a+" "+b);
+    System.out.println("After swap:");
+    swap(a,b);
+    }
+    
 }
