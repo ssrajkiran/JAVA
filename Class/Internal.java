05d8bd061861479333afee9ca7190678d852997c
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-12 16:21:31 +0530

    updated

--- Class/Internal.java
+++ Class/Internal.java
@@ -1,15 +1,43 @@
 import java.util.*;
 
-public class Internal
-{
-    static void Getmark()
-    {
-        
-        System.out.println("Your mark is : 20 ");
+public class Internal {
+  private Scanner input;
+  private String name;
+  private int id;
+  private int[] marks;
+
+  public Internal() {
+    input = new Scanner(System.in);
+    marks = new int[5];
+  }
+
+  public void getMarks() {
+    System.out.println("Enter marks for Tamil:");
+    marks[0] = input.nextInt();
+    System.out.println("Enter marks for English:");
+    marks[1] = input.nextInt();
+    System.out.println("Enter marks for Maths:");
+    marks[2] = input.nextInt();
+    System.out.println("Enter marks for Science:");
+    marks[3] = input.nextInt();
+    System.out.println("Enter marks for Social:");
+    marks[4] = input.nextInt();
+
+  }
+
+  public void getDetails() {
+    System.out.println("Enter your name:");
+    name = input.nextLine();
+    System.out.println("Enter your id:");
+    id = input.nextInt();
+  }
+
+  public void PrintMarks() {
+    System.out.println(String.format("name : %s id : %s", name, id));
+    System.out.println("*** Marks ***");
+    for (int i : marks) {
+      System.out.println("" + i);
     }
-    public static void main(String[] args)
-    {
-            Getmark();
-        
-    }
+  }
+
 }
--- Class/InternalMarks.java
+++ Class/InternalMarks.java
@@ -1,11 +1,9 @@
-import java.util.*;
 
-public class InternalMarks
-{
-
-    public static void main(String []args)
-    {
+public class InternalMarks {
+    public static void main(String[] args) {
         Internal ob1 = new Internal();
-        ob1.Getmark();
+        ob1.getDetails();
+        ob1.getMarks();
+        ob1.PrintMarks();
     }
 }
