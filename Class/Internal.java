d0db5b91723a4405883e3e53b27c38f3a5151e64
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-12 15:02:49 +0530

    just class added

---
+++ Class/Internal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Internal
+{
+    static void Getmark()
+    {
+        
+        System.out.println("Your mark is : 20 ");
+    }
+    public static void main(String[] args)
+    {
+            Getmark();
+        
+    }
+}
---
+++ Class/InternalMarks.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+Public class InternalMarks
+{
+
+    Public static void main(String []args)
+    {
+        Internal ob1 = new Internal();
+        ob1.getmarks();
+    }
+}
+
