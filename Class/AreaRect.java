1a94d314e4a6cc1d90dca78886ec3b3316ef88f3
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-13 19:47:22 +0530

    Added area

---
+++ Class/AreaRect.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class AreaRect extends result {
+        public static void main(String [] args)
+    {
+    result ob1 = new result();
+    ob1.setresult();
+    ob1.getresult();
+    
+    }
+}
+
+class result{
+     
+    static Scanner input = new Scanner (System.in);
+    private int num1;
+    private int num2;
+
+    
+
+    
+    public void setresult() {
+        System.out.println("Area of square and rectangle:");
+        System.out.println("Enter the number:");
+        num1 = input.nextInt();
+        System.out.println("Enter the number:");
+        num2 = input.nextInt();
+    }
+
+    public void getresult() {
+        
+    
+        System.out.println("Area of rectangle:" + 2 * num1 * num2);
+        System.out.println("Area of square:" + 4 * num1 * num1);
+
+        while (num1 != num2) {
+            System.out.println("Its rectangle");
+            break;
+        }
+        for (; num1 == num2;) {
+            System.out.println("Its square");
+            break;
+        }
+    }
+
+}
