863ca9b17eeab31f1eb1f5bd882cd9033f0c3885
ssrajkiran <ssrajkiran12gmail.com>
2020-07-20 12:31:04 +0530

    updated Power

---
+++ Method/Power.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+//import java.lang.Math;
+
+public class Power {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int result = 1;
+
+        System.out.println("Enter the number:");
+        int n = input.nextInt();
+        System.out.println("Enter the power of a number:");
+        int p = input.nextInt();
+        //System.out.println(Math.pow(n, p));
+        for (int i = p; i != 0; --i) {
+            result = result * n;
+        }
+        System.out.println(result);
+
+    }
+}
