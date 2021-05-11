1c44e75a63cc93ff83cb54a14ef58431f7b9d944
ssrajkiran <ssrajkiran1@gmail.com>
2021-05-12 00:56:21 +0530

    updated

--- Class/StudentMark/Mark.java
+++ Class/StudentMark/Mark.java
@@ -1,40 +1,57 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
-public class Mark
-{
+
+public class Mark {
     public int no;
+   
     public Scanner input = new Scanner(System.in);
-    public void get(){
 
-        System.out.println("Enter how many subject:");
-        no = input.nextInt();
-        int mark[] = new int[no];
-        System.out.println("Enter the mark of "+no+"Subjects :");
-        for (int i=0;i<no;i++){
-        mark[i] = input.nextInt();
-        }
-
-        System.out.println("Your marks are :");
-
-        for (int j=0;j<mark.length;j++){
-        System.out.println("Mark of"+j+"subject :"+mark[j]);
-        }
-        total(mark);
-    }
-        public void total(int [] array)
-        {
-            int sum=0;
-            for (int k=0;k<array.length;k++){
-                sum+=array[k];
+    public void get() {
+        boolean a =true;
+        while(a){
+            try{
+                System.out.println("Enter how many subjects:");
+                no=input.nextInt();
+                a=false;
+            }catch(InputMismatchException e){
+                System.out.println("Invalid");
+                input.next();
             }
-           System.out.println("Total subject mark :"+sum); 
-           average(sum,no);
         }
+        int mark[]= new int[no];
+        boolean b =true;
+        while(b){
+            try{
+                System.out.println("Enter the mark of " + no + "Subjects :");
+                for (int i = 0; i < no; i++) {
+                mark[i] = input.nextInt();
+        }
+        b=false;
+                }catch(InputMismatchException e){
+                    System.out.println("Invalid error");
+                    input.next();
+                }
+            }
+            System.out.println("Your marks are :");
 
+             for (int j = 0; j < mark.length; j++) {
+                     System.out.println("Mark of" + j + "subject :" + mark[j]);
+             }
+            total(mark);
+    }
 
-        public void average(int t, int no){
-            int average =t/no;
-            System.out.println("Your average mark:"+average);
-        } 
-        
-    
+    public void total(int[] array) {
+       int sum = 0;
+       for (int k = 0; k < array.length; k++) {
+           sum += array[k];
+       }
+        System.out.println("Total subject mark :" + sum);
+        average(sum, no);
+    }
+
+    public void average(int t, int no) {
+        int average = t / no;
+        System.out.println("Your average mark:" + average);
+    }
+
 }
