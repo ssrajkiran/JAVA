79b24309b311c1d599eebd14987e363ec148c5fc
ssrajkiran <ssrajkiran1@gmail.com>
2021-05-16 04:15:46 +0530

    javadatabase with sql

---
+++ Database/student.java
@@ -0,0 +1,79 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class student {
+    public static Scanner input = new Scanner(System.in);
+    private static Connection con;
+    private static Statement st;
+    private static ResultSet rs;
+    public static String name;
+    public static int id;
+    public static int mark[] = new int[5];
+    public static int sum = 0;
+    public static boolean n = true;
+
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println("Enter student name :");
+        name = input.nextLine();
+        while (n) {
+            try {
+                System.out.println("Enter student id :");
+                id = input.nextInt();
+
+                System.out.println("Enter student mark of 5 subject :");
+                for (int i = 0; i < 5; i++) {
+                    mark[i] = input.nextInt();
+                    sum += mark[i];
+                }
+
+                n = false;
+
+            } catch (InputMismatchException f) {
+                System.out.println("Error Input : Retry");
+                input.next();
+            }
+        }
+
+        int average = sum / 5;
+        dbconnect(name, id, mark, average);
+    }
+
+    public static void dbconnect(String name, int id, int[] mark, int average) throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "1234");
+            st = con.createStatement();
+            String q = "insert into Record values('" + name + "','" + id + "','" + mark[0] + "','" + mark[1] + "','"
+                    + mark[2] + "','" + mark[3] + "','" + mark[4] + "','" + average + "');";
+            System.out.println(q);
+            st.executeUpdate(q);
+
+            String q1 = "select name,id,Tamil,English,Maths,Science,Social,Average from record;";
+            rs = st.executeQuery(q1);
+            while (rs.next()) {
+                System.out.print(rs.getString("Name") + "\t\t");
+                System.out.print(rs.getInt("Id") + "\t\t");
+                System.out.print(rs.getInt("Tamil") + "\t\t");
+                System.out.print(rs.getInt("English") + "\t\t");
+                System.out.print(rs.getInt("Maths") + "\t\t");
+                System.out.print(rs.getInt("Science") + "\t\t");
+                System.out.print(rs.getInt("Social") + "\t\t");
+                System.out.print(rs.getInt("Average") + "\t\t");
+                System.out.println();
+            }
+            con.close();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException r) {
+            r.printStackTrace();
+        }
+
+    }
+}
