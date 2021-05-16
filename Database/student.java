841b29986214a7e7676c65c6f2413dbbed9839fe
ssrajkiran <ssrajkiran1@gmail.com>
2021-05-16 08:15:44 +0530

    update student

--- Database/student.java
+++ Database/student.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
 public class student {
     public static Scanner input = new Scanner(System.in);
     private static Connection con;
@@ -19,27 +20,51 @@ public class student {
 
     public static void main(String[] args) throws SQLException {
 
-        System.out.println("Enter student name :");
-        name = input.nextLine();
+        
+        
         while (n) {
             try {
-                System.out.println("Enter student id :");
-                id = input.nextInt();
-
-                System.out.println("Enter student mark of 5 subject :");
-                for (int i = 0; i < 5; i++) {
-                    mark[i] = input.nextInt();
-                    sum += mark[i];
+                System.out.println("Enter student name :");
+                if(input.hasNextInt()){
+                    System.out.println("Enter valid name :");
+                    input.next();
+                    name = input.next();
+                }else
+                {
+                    name = input.next();
                 }
 
+
+                System.out.println("Enter student id :");
+                if(input.hasNextInt()){
+                    id = input.nextInt();
+                }else{
+                    System.out.println("Enter valid id:");
+                    input.next();
+                    id=input.nextInt();
+                }
+                 System.out.println("Enter student mark of 5 subject :");
+
+                 for (int i = 0; i < 5; i++) {
+                    if (input.hasNextInt()) {
+                        mark[i] = input.nextInt();
+                        sum = sum +mark[i];
+                    }
+                    else{
+                        System.out.println("Enter valid marks");
+                        input.next();
+                        mark[i]=input.nextInt();
+                        sum = sum+mark[i];
+                    }
+                }
                 n = false;
 
             } catch (InputMismatchException f) {
-                System.out.println("Error Input : Retry");
-                input.next();
+                System.out.println("Error");
             }
+            
         }
-
+        System.out.println(sum);
         int average = sum / 5;
         dbconnect(name, id, mark, average);
     }
@@ -47,7 +72,7 @@ public class student {
     public static void dbconnect(String name, int id, int[] mark, int average) throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "1234");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root", "1234");
             st = con.createStatement();
             String q = "insert into Record values('" + name + "','" + id + "','" + mark[0] + "','" + mark[1] + "','"
                     + mark[2] + "','" + mark[3] + "','" + mark[4] + "','" + average + "');";
@@ -56,6 +81,7 @@ public class student {
 
             String q1 = "select name,id,Tamil,English,Maths,Science,Social,Average from record;";
             rs = st.executeQuery(q1);
+            System.out.println("NAME\t\tID\t\tTAMIL\t\tENGLISH\t\tMATHS\t\tSCIENCE\t\tSOCIAL\t\tAVERAGE");
             while (rs.next()) {
                 System.out.print(rs.getString("Name") + "\t\t");
                 System.out.print(rs.getInt("Id") + "\t\t");
